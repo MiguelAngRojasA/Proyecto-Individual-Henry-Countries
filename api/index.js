2e9bb13994263a9fcd90dcae41efd30ded4ea1d8
MiguelAngRojasA <miguelarojas05@unisalle.edu.co>
2023-05-09 16:17:35 -0500

    deply backend try 5

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
 
-// Syncing all the models at once.
+// Syncing all the models at once
 conn.sync({ alter: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('listening at', process.env.PORT); // eslint-disable-line no-console
