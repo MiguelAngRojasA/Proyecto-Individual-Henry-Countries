e6216e8344c49efb0d52c9b2369a411d85f1eb67
MiguelAngRojasA <miguelarojas05@unisalle.edu.co>
2023-05-09 20:16:00 -0500

    Without backend deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,12 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DATABASE_URL } = process.env;
-//  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_PORT}`, DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT,
+const {DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT, DATABASE_URL } = process.env;
 const axios = require("axios");
 const pg = require('pg');
 
-const sequelize = new Sequelize(DATABASE_URL,
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
